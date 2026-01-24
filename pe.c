2e6768ab6c2e5779d44a27c0813f157e6b350d0e
Heng Li <lh3@me.com>
2026-01-24 16:58:03 -0500

    ported bwa pairing
    bwa pairing is more general than minimap2 pairing. It is also more robust to
    overlapping ends. matesw will be next.

--- pe.c
+++ pe.c
@@ -86,42 +86,77 @@ void mb_pestat(void *km, const mb_opt_t *opt, int32_t n_frag, const int32_t *seg
 		kfree(km, q->a);
 	}
 }
-/*
-void mb_pair(void *km, const mb_opt_t *opt, const l2b_t *l2b, int32_t n_hit[2], mb_hit_t *hit[2], const mb_pestat_t pes[4])
+
+#define MB_SQRT1_2 0.707106781186547524401
+
+typedef struct {
+	int32_t score, sub_sc, n_sub, n_pp;
+	int32_t i[2];
+} mb_pairaux_t;
+
+void mb_pair(void *km, const mb_opt_t *opt, const l2b_t *l2b, int32_t n_hit[2], mb_hit_t *hit[2], const mb_pestat_t pes[4], mb_pairaux_t *ret)
 {
-	int32_t r, i, n_pa, y[4];
-	mb128_t *pa;
-	if (n_hit[0] + n_hit[1] == 0) return;
+	int32_t r, i, k, n_pa, y[4], n_pp = 0, m_pp = 0;
+	mb128_t *pa, *pp = 0; // pp: proper pairs
+
+	ret->i[0] = ret->i[1] = ret->score = ret->sub_sc = -1, ret->n_sub = 0;
+	if (n_hit[0] == 0 || n_hit[1] == 0) return;
 	pa = Kcalloc(km, mb128_t, n_hit[0] + n_hit[1]);
 	for (r = n_pa = 0; r < 2; ++r) {
 		for (i = 0; i < n_hit[r]; ++i) {
 			mb128_t *p = &pa[n_pa++];
 			const mb_hit_t *h = &hit[r][i];
-			p->x = l2b->ctg[h->tid].off + h->ts;
+			p->x = l2b->ctg[h->tid].off + (h->rev? h->te : h->ts);
 			p->y = (uint64_t)i << 2 | (uint64_t)h->rev << 1 | r;
 		}
 	}
 	radix_sort_mb128x(pa, pa + n_pa);
+
 	y[0] = y[1] = y[2] = y[3] = -1;
 	for (i = 0; i < n_pa; ++i) {
 		mb128_t *pi = &pa[i];
-		mb_hit_t *hi = &hit[pi->y&1][pi->y>>2];
+		const mb_hit_t *hi = &hit[pi->y&1][pi->y>>2];
 		for (r = 0; r < 2; ++r) {
 			int which, dir = r << 1 | (pi->y>>1&1);
 			if (pes[dir].failed) continue; // invalid orientation
 			which = r << 1 | ((pi->y&1) ^ 1);
 			if (y[which] < 0) continue; // no previous hit
 			for (k = y[which]; k >= 0; --k) {
-				mb128_t *pk = &pa[k];
-				mb_hit_t *hk = &hit[pk->y&1][pk->y>>2];
+				mb128_t *pk = &pa[k], *q;
+				const mb_hit_t *hk = &hit[pk->y&1][pk->y>>2];
 				int64_t dist;
+				double ns, s;
 				if ((pk->y&3) != which) continue;
 				if (hi->tid != hk->tid) break;
-				dist = hi->ts - hk->ts;
-				if (dist >= 
+				dist = pi->x - pk->x;
+				if (dist > pes[dir].high) break;
+				if (dist < pes[dir].low) continue;
+				ns = (dist - pes[dir].avg) / pes[dir].std; // normalized score
+				s = hk->p->dp_max + hi->p->dp_max + .721 * log(2. * erfc(fabs(ns) * MB_SQRT1_2)) * opt->a; // .721 = 1/log(4)
+				if (s < 0.) s = 0.;
+				if (n_pp == m_pp) Kgrow(km, mb128_t, pp, n_pp, m_pp);
+				q = &pp[n_pp++];
+				q->y = (pk->y&1) == 0? (uint64_t)k << 32 | i : (uint64_t)i << 32 | k; // upper bits: index to read[0]; lower: to read[1]
+				q->x = (uint64_t)(s + .499) << 32 | ((hk->hash ^ hi->hash) & 0xffffffffULL);
 			}
 		}
+		y[pi->y&3] = i;
 	}
+
+	ret->n_pp = n_pp;
+	if (n_pp > 0) {
+		uint64_t max = 0, max2 = 0;
+		int32_t tmp = opt->a + opt->b > opt->q + opt->e? opt->a + opt->b : opt->q + opt->e;
+		for (i = 0; i < n_pp; ++i) { // find max and max2
+			mb128_t *q = &pp[i];
+			if (q->x > max) max = q->x, ret->i[0] = q->y>>32, ret->i[1] = (uint32_t)q->y;
+			else if (q->x > max2) max2 = q->x;
+		}
+		ret->score = max>>32, ret->sub_sc = max2>>32;
+		for (i = 0; i < n_pp; ++i)
+			if (pp[i].x>>32 <= ret->score - tmp)
+				ret->n_sub++;
+	}
+	kfree(km, pp);
 	kfree(km, pa);
 }
-*/
